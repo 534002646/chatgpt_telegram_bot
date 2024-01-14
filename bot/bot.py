d17b5f9f2717aeacd1cf2974ab0b19885bfe7cab
Vincent <534002646@qq.com>
2024-01-14 20:04:34 +0800

    bot

--- bot/bot.py
+++ bot/bot.py
@@ -793,7 +793,7 @@ def run_bot() -> None:
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & user_filter, message_handle))
     application.add_handler(MessageHandler(filters.VOICE & user_filter, voice_message_handle))
-    application.add_handler(MessageHandler((filters.PHOTO | filters.VIDEO) & user_filter, photo_message_handle))
+    application.add_handler(MessageHandler(filters.PHOTO & user_filter, photo_message_handle))
     
     application.add_handler(CommandHandler("audio", generate_audio_handle, filters=user_filter))
     application.add_handler(CommandHandler("img", generate_image_handle, filters=user_filter))
